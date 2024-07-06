a033b9727d93ad2390b5f0db880eb40d0a639a3c
Revy <162366071+teoto4@users.noreply.github.com>
2024-07-06 13:53:20 +0300

    first commit

---
+++ js/main_js.js
@@ -0,0 +1,41 @@
+import { languageSelect } from "./transelte.js";
+
+const en_lang = document.getElementById("en_lang");
+const es_lang = document.getElementById("es_lang");
+const fr_lang = document.getElementById("fr_lang");
+const all_lang = ["en", "es", "fr"];
+
+//Вибір мови
+// for (let key of languageSelect) {
+//   console.log(key);
+// }
+en_lang.addEventListener("click", (en_lang) => {
+  location.href = window.location.pathname + "#" + "en";
+  location.reload();
+});
+es_lang.addEventListener("click", (es_lang) => {
+  location.href = window.location.pathname + "#" + "es";
+  location.reload();
+});
+fr_lang.addEventListener("click", (fr_lang) => {
+  location.href = window.location.pathname + "#" + "fr";
+  location.reload();
+});
+
+//Зміна мови
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substr(1);
+  console.log(hash);
+  if (!all_lang.includes(hash)) {
+    location.href = window.location.pathname + "#en";
+    location.reload();
+  }
+
+  document.querySelector(".lang-partner-text").innerHTML =
+    languageSelect["partner-text"][hash];
+  document.querySelector(".lang-food-text").innerHTML =
+    languageSelect["food-text"][hash];
+}
+
+changeLanguage();
---
+++ js/transelte.js
@@ -0,0 +1,12 @@
+export const languageSelect = {
+  "partner-text": {
+    en: "Restaurateur? Join the Taster Platform and rapidly launch one of our iconic virtual restaurant brands from your existing kitchen!",
+    es: "¿Restaurador? ¡Únase a Taster Platform y lance rápidamente una de nuestras icónicas marcas de restaurantes virtuales desde su cocina actual!",
+    fr: "Restaurateur? Rejoignez la plateforme Taster et lancez rapidement l'une de nos marques de restaurant virtuel emblématiques depuis votre cuisine existante!",
+  },
+  "food-text": {
+    en: "The best of street food at home",
+    es: "Lo mejor de la comida callejera en casa.",
+    fr: "Le meilleur de la street food à la maison",
+  },
+};
